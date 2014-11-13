2ff2894941c6b61b72b20d23e321d69bd595acf3
jas0n <jasonkuan87@gmail.com>
2014-11-13 11:46:04 +0100

    connected to riak

--- Frontend/hashBubble.php
+++ Frontend/hashBubble.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 require_once 'riak-php-client/src/Basho/Riak/Riak.php';
 require_once 'riak-php-client/src/Basho/Riak/Bucket.php';
@@ -13,36 +12,85 @@ require_once 'riak-php-client/src/Basho/Riak/Link/Phase.php';
 require_once 'riak-php-client/src/Basho/Riak/MapReduce/Phase.php';
 
 
-// Class definitions for our models
+# Starting Client
+$client = new Basho\Riak\Riak('127.0.0.1',8098);
 
-class Tweet
-{
-    var $tweetId;
-    var $text;
-}
-
-// Creating Data
-$tweet = new Tweet();
-$tweet->tweetId = 1;
-$tweet->text = 'This is a tweet';
-
-// Starting Client
-$client = new Basho\Riak\Riak('127.0.0.1', 10018);
-
-// Creating Buckets
-$tweetBucket = $client->bucket('Tweets');
-
-// Storing Data
-$tweet_riak = $tweetBucket->newObject(
-    strval($tweet->tweetId), $tweet
-);
-$tweet_riak->store();
+# Choose Bucket
+$tweetBucket = $client->bucket('tweets');
 
 // Fetching related data by shared key
-$fetched_data = $tweets->get('1')->data;
-$fetched_data['Tweets'] = $tweetBucket->get('1')->data;
+$fetched_tweet = $tweetBucket->get('keys')->data['text'];
 print("Tweet: \n");
 print_r($fetched_tweet);
 
 
+
+// # Choose Bucket
+// $customerBucket = $client->bucket('Customers');
+
+// // Fetching related data by shared key
+// $fetched_customer = $customerBucket->get('1')->data['address'];
+// print("Desired Data: \n");
+// print_r($fetched_customer);
+
+
+
+
+
+
+// # Supply a key under which to store your data
+// $person = $bucket->newObject('riak_dev', array(
+//     'name' => "Jay Jackson",
+//     'age' => 72,
+//     'company' => "WhoringGalour"
+// ));
+// # Create some test data
+// $bucket = $client->bucket("searchbucket");
+// $bucket->newObject("one", array("foo"=>"one", "bar"=>"red"))->store();
+// $bucket->newObject("two", array("foo"=>"two", "bar"=>"green"))->store();
+
+// # Execute a search for all objects with matching properties
+// $results = $client->getData("one" | "bar")->run();
+
+// print_r($results);
+
+
+
+
+
+
+
+
+// # Save the object to Riak
+// $person->store();
+
+# Fetch the object
+// $person = $bucket->get('riak_dev');
+
+
+// Fetching related data by shared key
+// $fetched_data = $bucket->get('riak_dev')->data;
+// $fetched_data['riak_dev'] = $bucket->get('riak_dev')->data;
+// print("Customer Info: \n");
+// print_r($fetched_tweet);
+
+
+
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
