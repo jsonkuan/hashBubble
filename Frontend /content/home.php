899ee1a805402e6689c6e6a44d45c44225acb58a
jas0n <jasonkuan87@gmail.com>
2014-11-27 15:43:04 +0100

    added buttons

--- Frontend /content/home.php
+++ Frontend /content/home.php
@@ -1,6 +1,3 @@
-<?php ##home page content ?>
-
-
 <!DOCTYPE html> 
 <html lang ="en">
 <head>
@@ -11,16 +8,19 @@
 <body>
    <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="false"></div>
  
-	<form class="form-wrapper cf" name="searchform" action="hashBubble.php" method="post">
+	<form class="form-wrapper cf" name="searchform" action="" method="post">
 		<input type="text" name="search" placeholder="Enter a #hashtag">
 		<button type="submit" value="Search">Find#</button>
 	</form>
+	
+	<!-- Bubbles that float around, and hide N seconds after clicked -->
+	<button class="bubble" id="b1"></button>
+	<button class="bubble" id="b2"></button>
+	<button class="bubble" id="b3"></button>
 
-<?php include 'hashBubble.php';?>
-  
 
-    </form> 
- 
+
+	<?php include('hashBubble.php');?>
  </body>
  </html> 
  
