79d7ef8243b5b5e3ab154348b3d82c55cbb4c1ea
jas0n <jasonkuan87@gmail.com>
2014-11-27 15:26:14 +0100

    Minior improvements

--- Frontend /index.php
+++ Frontend /index.php
@@ -1,7 +1,5 @@
 <?php
 include('setup.php');
-
-
 ?>
 
 <! DOCTYPE HTML>
@@ -15,58 +13,7 @@ include('setup.php');
 </head>
 <body>
 	
-	 <script>
-	  // This is called with the results from from FB.getLoginStatus().
-  	function statusChangeCallback(response) {
-	    console.log('statusChangeCallback');
-	    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '293662900827630',
-	      xfbml      : true,
-	      version    : 'v2.2'
-	    });
-	  };
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
-	<body background="bubbles".jpg">
 	<div class="header">
-
 		<?php include('template/header.php'); ?>
 	</div>
 	
@@ -75,14 +22,11 @@ include('setup.php');
 	</div>
 
 	<div class="content ">
-
 		<?php include('content/'.$page.'.php'); ?>
 	</div>
 
 	<div class="footer">
-		<p> © Hash Bubble Inc 2014 </p> 
+		<p> © HashBubble Inc 2014 </p> 
 	</div>
-
-
 </body>
 <html>
--- Frontend /setup.php
+++ Frontend /setup.php
@@ -1,10 +1,10 @@
 <?php
 //setup doc
 
-if ($_GET['page'] == '') {
+if (!isset($_GET['page'])) {
 
 	$page = 'home';
-
+	
 } else {
 
 	$page = $_GET['page'];
