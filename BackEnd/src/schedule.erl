ca8176948ab7816d4bd99552551f267133f87f58
regine22 <regine.aquino22@yahoo.com.ph>
2014-12-03 16:45:09 +0100

    schedule change

--- BackEnd/src/schedule.erl
+++ BackEnd/src/schedule.erl
@@ -8,19 +8,19 @@ start() ->
 	erlang:display("timer started"),
 	start(erlang:localtime()).
 
-%	start({_Date, {_, _, 00}}) ->
-%	twitterserver:get_tweets(),
-%	CurrentTime = erlang:localtime(),
-%	erlang:display("i am sleeping now!, love twitter"),
-%	timer:sleep(1000),
-%	start(CurrentTime);
-
-start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
+start({_Date, {_, _, 00}}) ->
 	twitterserver:get_tweets(),
 	CurrentTime = erlang:localtime(),
-	erlang:display("goodnight instagram!"),
+	erlang:display("i am sleeping now!, love twitter"),
 	timer:sleep(1000),
 	start(CurrentTime);
+
+%start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
+%	twitterserver:get_tweets(),
+%	CurrentTime = erlang:localtime(),
+%	erlang:display("goodnight instagram!"),
+%	timer:sleep(1000),
+%	start(CurrentTime);
 
 start({_Date, _Time}) ->
 	CurrentTime = erlang:localtime(),
