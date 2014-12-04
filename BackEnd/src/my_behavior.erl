a376c0672b5ac83af6c3867abe7c458bafd3b739
NicoleMusco94 <nicole.m.musco@gmail.com>
2014-12-04 11:44:51 +0100

    added callback handle_call to twitterserver

--- BackEnd/src/my_behavior.erl
+++
@@ -1,6 +0,0 @@
--module(my_behavior).
--export([behavior_info/1]).
-
-%%init, are now expected callbacks
-behavior_info(callbacks)-> [{init,1}, {handle_info}, {handle_cast,2}, {terminate,2}, {code_Change,3}, {handle_call,3}];
-behavior_info(_)-> undefined.
