2393cb45d2670e4816f68c257f4f583ff4309fbb
NicoleMusco94 <nicole.m.musco@gmail.com>
2014-12-04 10:01:55 +0100

    changed to using separate insta/tweet pulling

--- BackEnd/src/schedule.erl
+++ BackEnd/src/schedule.erl
@@ -15,18 +15,14 @@ start({_Date, {_, _, 00}}) ->
 	timer:sleep(1000),
 	start(CurrentTime);
 
-%start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
-%	twitterserver:get_tweets(),
-%	CurrentTime = erlang:localtime(),
-%	erlang:display("goodnight instagram!"),
-%	timer:sleep(1000),
-%	start(CurrentTime);
+start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
+	twitterserver:get_insta(),
+	CurrentTime = erlang:localtime(),
+	erlang:display("goodnight instagram!"),
+	timer:sleep(1000),
+	start(CurrentTime);
 
 start({_Date, _Time}) ->
 	CurrentTime = erlang:localtime(),
 	timer:sleep(1000),
 	start(CurrentTime).
-
-
-stop() -> 
-	{ok, stopped_schedule}.
