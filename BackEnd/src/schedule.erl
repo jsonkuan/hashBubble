a2c80bccc8e43b4868a4679ee2de9f4cd9d590d2
NicoleMusco94 <nicole.m.musco@gmail.com>
2014-12-04 11:16:00 +0100

    reverting back to previous version
     changes failed

--- BackEnd/src/schedule.erl
+++ BackEnd/src/schedule.erl
@@ -2,12 +2,16 @@
 
 
 -module(schedule).
--export([start/0]).
+-export([start/0, stop/0]).
 
+
+%%schedule used for restarting twitter feed every minute and 
+%%restarting the instagram feed every five seconds (only one object at a time)
 start() -> 
 	erlang:display("timer started"),
 	start(erlang:localtime()).
 
+%%every new minute, get tweets then sleep for a second, start over
 start({_Date, {_, _, 00}}) ->
 	twitterserver:get_tweets(),
 	CurrentTime = erlang:localtime(),
@@ -15,14 +19,20 @@ start({_Date, {_, _, 00}}) ->
 	timer:sleep(1000),
 	start(CurrentTime);
 
-start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
-	twitterserver:get_insta(),
-	CurrentTime = erlang:localtime(),
-	erlang:display("goodnight instagram!"),
-	timer:sleep(1000),
-	start(CurrentTime);
+%%every 5 seconds, get instagram data then sleep for a second, start over
+%start({_Date, {_,_, S}}) when S rem 5 =:= 0 ->
+%	twitterserver:get_insta(),
+%	CurrentTime = erlang:localtime(),
+%	erlang:display("goodnight instagram!"),
+%	timer:sleep(1000),
+%	start(CurrentTime);
 
+%%if no pattern matches, sleep for a second then start over
+%%the purpose of this is to prevent the schedule from stopping if the other patterns dont match
 start({_Date, _Time}) ->
 	CurrentTime = erlang:localtime(),
 	timer:sleep(1000),
 	start(CurrentTime).
+%%to stop schedule if we want	
+stop() -> 
+	{ok, stopped_schedule}.
