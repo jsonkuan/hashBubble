d9945030c43e06dbdd1e690fe5541cf5a06ce7b6
michaelgwarne <michaelgwarne@gmail.com>
2014-12-14 14:18:47 +0100

    added scripts and mapreduce config file

---
+++ BackEnd/scripts/riak_start.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash  
+sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev1/bin/riak start
+sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev2/bin/riak start
+sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev3/bin/riak start
+sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev4/bin/riak start
+sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev5/bin/riak start
