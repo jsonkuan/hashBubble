285068bb93022d1764e121a2363f0d08100cccd8
michaelgwarne <michaelgwarne@gmail.com>
2014-12-14 15:00:08 +0100

    improved scripts

--- BackEnd/scripts/riak_restart.sh
+++ BackEnd/scripts/riak_restart.sh
@@ -1,6 +1,3 @@
  #!/bin/bash  
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev1/bin/riak-admin erl-reload 
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev2/bin/riak-admin erl-reload 
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev3/bin/riak-admin erl-reload 
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev4/bin/riak-admin erl-reload 
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev5/bin/riak-admin erl-reload 
+
+for node in ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev*; do $node/bin/riak-admin erl-reload; done
--- BackEnd/scripts/riak_start.sh
+++ BackEnd/scripts/riak_start.sh
@@ -1,6 +1,3 @@
  #!/bin/bash  
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev1/bin/riak start
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev2/bin/riak start
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev3/bin/riak start
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev4/bin/riak start
-sudo ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev5/bin/riak start
+
+for node in ~/otp_src_R16B02-basho5/riak-2.0.2/dev/dev*; do $node/bin/riak start; done
