dd87b9c7aee5956083df2983453ca9f3850f0ea4
jas0n <jasonkuan87@gmail.com>
2014-12-16 15:19:32 +0100

    Images appear in bubbles

--- Frontend/src/second.php
+++ Frontend/src/second.php
@@ -6,51 +6,60 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/bubbles.css"> 
     <link rel="stylesheet" href="css/styles.css"> 
+   
    <!-- JQuery Source -->
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
  
- 	<div class="header">
+ 	<!-- Home Button, redirects to main.php -->
+    <div class="header">
 	  <style>
-	    html.no-svg .svg-image { display: none }
+	   html.no-svg .svg-image { display: none }
 	  </style>
 	  <a href="./main.php">
 	    <img class="svg-image" src="home.svg" height="55" width="55">
 	  </a>
-
-
  	</div>
     
-  <!-- Search bar -->
-  <div id="searchtext_2">							
-    <form class="form-wrapper cf" name="searchform" action="" method='post' id="searchtext_2">
+    <!-- Search bar -->
+    <div id="searchtext_2">							
+      <form class="form-wrapper cf" name="searchform" action="" method='post' id="searchtext_2">
         <input type="text" name="search" placeholder="Enter a #hashtag">
         <button type="submit" value="Search">Find#</button >
-    </form>
+      </form>
     </div>
 </head>
 
 <body>
 
-<!-- Uses var $url from riakQuery and Displays as Img -->
+<!-- Passes $url from riakQuery and Displays url as image -->
 <script type="text/javascript">
   var bubbleUrl = <?php echo json_encode($url); ?>;
 
-    function showContent(bubbleUrl) {
+    function showContent(bubbleUrl, getElementById) {
         var src = bubbleUrl;
-        show_image(bubbleUrl, 250,200, "Image: Lost in translation");
+        show_image(bubbleUrl, 200,200, getElementById);
+        
     }
-
-    function show_image(src, width, height, alt) {
+    function show_image(src, width, height, id) {
         var img = document.createElement("img");
         img.src = src;
         img.width = width;
-        img.height = height;
-        img.alt = alt;
-        document.body.appendChild(img);
+        document.getElementById(id).appendChild(img);
     }
 </script>  
 
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#bub1").fadeOut(5000);
+    $("#bub2").fadeOut(5000);
+    $("#bub3").fadeOut(5000);
+  });
+});
+</script>
+
+<!-- <div id="div3" style="width:80px;height:80px;background-color:blue;"></div> -->
 
     <!-- Randomize Bubble Location on Screen -->
 <script type="text/javascript">
@@ -59,17 +68,15 @@
         var ww = $(window).width();
         var wh = $(window).height();
         $(".bubble").each(function(i){
-            var rotationNum=Math.round((Math.random()*360)+1);
-            var rotation="rotate("+rotationNum+"deg)";
-            var posx = Math.round(Math.random() * ww)-20;
-            var posy = Math.round(Math.random() * wh)-20;
-        $(this).css("top", posy + "px").css("left", posx + "px").css("transform",rotation).css("-ms-transform",rotation).css("-webkit-transform",rotation);
+            var posx = Math.round(Math.random() * ww);
+            var posy = Math.round(Math.random() * wh);
+        $(this).css("top", posy + "px").css("left", posx + "px").css
          });
     });
 </script>
 
-
-
-
 </body>
 </html>
+
+
+
