bd3815c1342c95681102134858923586c41a783a
jas0n <jasonkuan87@gmail.com>
2014-12-19 15:25:01 +0100

    Updated search action

--- Frontend/src/main.php
+++ Frontend/src/main.php
@@ -10,7 +10,7 @@
 
 <body> 
  <!-- Search bar -->
-    <form class="form-wrapper cf" name="searchform" id="searchtext" action="second.php" method='post'>
+    <form class="form-wrapper cf" name="searchform" id="searchtext" action="results.php" method='post'>
         <input type="text" name="search" placeholder="Enter a #hashtag">
         <button type="submit" value="Search">Find#</button >
     </form>
