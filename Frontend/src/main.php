089ba87a007d4e001260d4ea9c28cfb6096206a7
jas0n <jasonkuan87@gmail.com>
2015-01-08 19:24:28 +0100

    updated

--- Frontend/src/main.php
+++ Frontend/src/main.php
@@ -12,7 +12,7 @@
 <body>
 <!-- Search bar -->
 	<form class="form-wrapper cf" name="searchform" id="searchtext" action="results.php" method='post'>
-		<input type="text" name="search" placeholder="Enter a #hashtag">
+		<input type="text" name="search" placeholder="Search for a #hashtag">
 		<button type="submit" value="Search">Find#</button >
 	</form>
 
